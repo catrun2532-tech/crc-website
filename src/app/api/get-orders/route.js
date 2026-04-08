93927a4db0c9054035a808bc1cefeaffee062dbf
CRC Shop <catrun2532@gmail.com>
2026-04-08 09:14:41 -0700

    fix get-orders conflict clean

--- src/app/api/get-orders/route.js
+++ src/app/api/get-orders/route.js
@@ -2,31 +2,19 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodb";
 
 export async function GET() {
-<<<<<<< HEAD
-  const db = (await clientPromise).db("repair");
-
-  const data = await db
-    .collection("orders")
-    .find()
-    .sort({ _id: -1 }) // 🔥 เอาอันล่าสุดขึ้นบน
-    .toArray();
-
-  return NextResponse.json(data);
-=======
   try {
     const client = await clientPromise;
     const db = client.db("repair");
 
-    const orders = await db
+    const data = await db
       .collection("orders")
       .find({})
-      .sort({ _id: -1 })
+      .sort({ _id: -1 }) // เอาอันล่าสุดขึ้นบน
       .toArray();
 
-    return NextResponse.json(orders);
+    return NextResponse.json(data);
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return NextResponse.json({ error: "โหลดข้อมูลไม่สำเร็จ" });
   }
->>>>>>> 7bc84aa (fix tailwind config)
 }
