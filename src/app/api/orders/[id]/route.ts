5b93f902ab6551830bc9bad08acd9c49b4fe1f67
CRC Shop <catrun2532@gmail.com>
2026-04-09 06:26:41 -0700

    fix params promise

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -2,30 +2,43 @@ import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Order from "@/models/Order";
 
+// GET
 export async function GET(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
+
   await connectDB();
-  const data = await Order.findById(params.id);
+  const data = await Order.findById(id);
+
   return NextResponse.json(data);
 }
 
+// PUT
 export async function PUT(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
+
   await connectDB();
   const body = await req.json();
-  const updated = await Order.findByIdAndUpdate(params.id, body, { new: true });
+
+  const updated = await Order.findByIdAndUpdate(id, body, { new: true });
+
   return NextResponse.json(updated);
 }
 
+// DELETE
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: Promise<{ id: string }> }
 ) {
+  const { id } = await context.params;
+
   await connectDB();
-  await Order.findByIdAndDelete(params.id);
+  await Order.findByIdAndDelete(id);
+
   return NextResponse.json({ message: "Deleted" });
 }
