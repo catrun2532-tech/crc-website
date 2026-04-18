44336f98f5ecbb1f19e50293c8c9a938127b4057
CRC Shop <catrun2532@gmail.com>
2026-04-18 05:06:10 -0700

    fix layout merge conflict

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,19 @@
 import "./globals.css";
-<<<<<<< HEAD
-
-export const metadata = {
-  title: "C.R.C. คอมพิวเตอร์",
-  description: "รับซ่อมคอมพิวเตอร์ โน๊ตบุ๊ค อัปเกรด SSD RAM กู้ข้อมูล",
-};
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-=======
 import type { Metadata } from "next";
 import MusicPlayer from "../components/MusicPlayer";
 
 export const metadata: Metadata = {
   title: "C.R.C. คอมพิวเตอร์",
-  description: "เว็บไซต์ร้านซ่อมคอมพิวเตอร์",
+  description: "รับซ่อมคอมพิวเตอร์ โน๊ตบุ๊ค อัปเกรด SSD RAM กู้ข้อมูล",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
->>>>>>> fix-work
   return (
     <html lang="th">
       <body>
         {children}
-<<<<<<< HEAD
-      </body>
-    </html>
-  );
-}
-=======
         <MusicPlayer />
       </body>
     </html>
   );
 }
->>>>>>> fix-work
