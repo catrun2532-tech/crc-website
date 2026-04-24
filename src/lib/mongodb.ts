98b7880904da944a05cce371b0d01bf4d1d48db7
catrun2532-tech <catrun2532@gmail.com>
2026-04-23 22:00:15 -0700

    update next version

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,8 +1,50 @@
-import { MongoClient } from "mongodb"
+import mongoose from "mongoose";
 
-const uri = process.env.MONGODB_URI!
-const client = new MongoClient(uri)
+const MONGODB_URI = process.env.MONGODB_URI;
 
-const clientPromise = client.connect()
+if (!MONGODB_URI) {
+  throw new Error("❌ Please define MONGODB_URI in .env or Vercel");
+}
 
-export default clientPromise
+// type cache
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+// ใช้ global cache (กัน reconnect ตอน dev)
+const globalWithMongoose = globalThis as typeof globalThis & {
+  _mongoose?: MongooseCache;
+};
+
+const cached = globalWithMongoose._mongoose || {
+  conn: null,
+  promise: null,
+};
+
+if (!globalWithMongoose._mongoose) {
+  globalWithMongoose._mongoose = cached;
+}
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI!, {
+      bufferCommands: false,
+    });
+  }
+
+  try {
+    cached.conn = await cached.promise;
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    cached.promise = null;
+    console.error("❌ MongoDB error:", err);
+    throw err;
+  }
+
+  return cached.conn;
+}
+
+export default connectDB;
